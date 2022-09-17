ef4b3544ad84435a6e3ea042c581c8b834a8b1aa
Chirag Jain <dungarwalsaab@gmail.com>
2022-09-17 13:42:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+// function App() {
+//   return (
+//      <div>Helloo</div>
+//   );
+// }
+
+ 
+// export default App;
+
+
+ 
+// import Signup from './components/Signup'
+// import Login from './components/Login';
+import {useContext } from 'react';
+import { BrowserRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom';
+// import {AuthProvider, AuthContext} from './context/AuthContext'
+// import PrivateRoute from './components/PrivateRoute';
+// import Profile from './components/Profile';
+
 
 function App() {
+  // const { currentUser } = useContext(AuthContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <AuthProvider> */}
+        <Switch>
+            {/* <Route path="/" component={Login}/>
+            <Route path="/signup" component={Signup}/> */}
+            
+            
+            {/* <PrivateRoute path="/profile/:id" component={Profile}/>
+            <PrivateRoute path="/" component={Feed}/> */}
+      </Switch>
+      {/* </AuthProvider> */}
+     
+    </Router>
+    
+
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you nee
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/storage';
+import 'firebase/compat/firestore'; 
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBmFVDT4bQhhvD3HIDo7j-Yx1GzsK8AUkQ",
+    authDomain: "caladrius-tech-a-thon.firebaseapp.com",
+    projectId: "caladrius-tech-a-thon",
+    storageBucket: "caladrius-tech-a-thon.appspot.com",
+    messagingSenderId: "611043656305",
+    appId: "1:611043656305:web:e74afd67de4fe9b1ac11d0"
+  };
+
+
+export const app = firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+
+const firestore = firebase.firestore();
+
+export const databse = {
+   admin: firestore.collection('admin'),
+   users : firestore.collection('users'),
+}
+
+export const storage =firebase.storage();
